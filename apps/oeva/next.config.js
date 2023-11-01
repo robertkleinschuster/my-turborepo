fce773434aa95c23adb0f7158bfd275cb9958c2a
robertkleinschuster <git@robs.social>
2023-11-01 17:54:52 +0100

    remove hafas-client from transpile packages

--- apps/oeva/next.config.js
+++ apps/oeva/next.config.js
@@ -7,7 +7,7 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 
 module.exports = withPWA({
   reactStrictMode: true,
-  transpilePackages: ["ui", "konsta", "hafas-client"],
+  transpilePackages: ["ui", "konsta"],
   experimental: {
     serverComponentsExternalPackages: ["hafas-client"]
   },

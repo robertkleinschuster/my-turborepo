2cae0372c31b9ecb2386f2b6192f9fa2bed02296
robertkleinschuster <git@robs.social>
2023-12-10 18:59:55 +0100

    update dependencies dkice.svg for splashscreen

--- apps/oeva/app/app/layout.tsx
+++ apps/oeva/app/app/layout.tsx
@@ -1,41 +1,45 @@
-import type { Metadata, Viewport } from "next";
+import type {Metadata, Viewport} from "next";
+import React from "react";
 import AppLayout from "../../components/app-layout";
 
 export const metadata: Metadata = {
-  title: "OeVA",
-  description: "OeVA",
-  robots: {
-    index: false,
-    follow: false
-  },
-  appleWebApp: {
-    capable: true,
-    statusBarStyle: "black-translucent",
-    title: 'OeVA',
-    startupImage: [
-      { url: 'splashscreen/iphone-12-pro-max', media: 'screen and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)' },
-    ],
-  },
+    title: "OeVA",
+    description: "OeVA",
+    robots: {
+        index: false,
+        follow: false
+    },
+    appleWebApp: {
+        capable: true,
+        statusBarStyle: "black-translucent",
+        title: 'OeVA',
+        startupImage: [
+            {
+                url: 'splashscreen/iphone-12-pro-max',
+                media: 'screen and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
+            },
+        ],
+    },
 };
 
 export const viewport: Viewport = {
-  width: "device-width",
-  userScalable: false,
-  initialScale: 1,
-  minimumScale: 1,
-  maximumScale: 1,
-  viewportFit: "cover",
-  themeColor: '#000000',
+    width: "device-width",
+    userScalable: false,
+    initialScale: 1,
+    minimumScale: 1,
+    maximumScale: 1,
+    viewportFit: "cover",
+    themeColor: '#000000',
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }): JSX.Element {
-  return (
-    <AppLayout>
-      {children}
-    </AppLayout>
-  );
+    return (
+        <AppLayout>
+            {children}
+        </AppLayout>
+    );
 }
--- apps/oeva/app/layout.tsx
+++ apps/oeva/app/layout.tsx
@@ -1,45 +1,49 @@
-import { Metadata, Viewport } from "next";
+import type {Metadata, Viewport} from "next";
 import "./globals.css";
 import "../fonts/Betreiber-Logos/css/Betreiber-Logos.css"
 import "../fonts/Zuggattungen/css/Zuggattungen.css"
+import React from "react";
 
 export const metadata: Metadata = {
-  title: "OeVA",
-  description: "OeVA",
-  robots: {
-    index: false,
-    follow: false
-  },
-  appleWebApp: {
-    capable: true,
-    statusBarStyle: "black-translucent",
-    title: 'OeVA',
-    startupImage: [
-      { url: 'splashscreen/iphone-12-pro-max', media: 'screen and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)' },
-    ],
-  },
+    title: "OeVA",
+    description: "OeVA",
+    robots: {
+        index: false,
+        follow: false
+    },
+    appleWebApp: {
+        capable: true,
+        statusBarStyle: "black-translucent",
+        title: 'OeVA',
+        startupImage: [
+            {
+                url: 'splashscreen/iphone-12-pro-max',
+                media: 'screen and (device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)'
+            },
+        ],
+    },
 };
 
 export const viewport: Viewport = {
-  width: "device-width",
-  userScalable: false,
-  initialScale: 1,
-  minimumScale: 1,
-  maximumScale: 1,
-  viewportFit: "cover",
-  themeColor: '#000000',
+    width: "device-width",
+    userScalable: false,
+    initialScale: 1,
+    minimumScale: 1,
+    maximumScale: 1,
+    viewportFit: "cover",
+    themeColor: '#000000',
 }
 
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
+                                       children,
+                                   }: {
+    children: React.ReactNode;
 }): JSX.Element {
-  return (
-    <html lang="en">
-      <body>
+    return (
+        <html lang="en">
+        <body>
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }

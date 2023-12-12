a3e0e5fdbf3b39e2b2aa1b0906a4009e21a94c42
robertkleinschuster <git@robs.social>
2023-12-12 21:53:39 +0100

    fix light mode background

--- apps/oeva/components/app-layout.tsx
+++ apps/oeva/components/app-layout.tsx
@@ -1,14 +1,14 @@
 "use client"
 
-import { App } from "konsta/react"
+import {App} from "konsta/react"
 import React from "react"
 import Navigation from "./navigation"
 
-export default function AppLayout({ children }: { children: React.ReactNode }): React.JSX.Element {
+export default function AppLayout({children}: { children: React.ReactNode }): React.JSX.Element {
     return <App className="flex flex-col !min-h-full" id="app" safeAreas theme="ios">
-        <main className="flex flex-col flex-grow overflow-hidden">
+        <main className="flex flex-col flex-grow overflow-hidden bg-ios-light-surface dark:bg-ios-dark-surface">
             {children}
         </main>
-        <Navigation />
+        <Navigation/>
     </App>
 }

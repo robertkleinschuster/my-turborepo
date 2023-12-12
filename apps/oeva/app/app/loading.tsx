85018771ad79fa47c3dbf960fa71bb7b0088cade
robertkleinschuster <git@robs.social>
2023-12-12 22:53:13 +0100

    global loading component

---
+++ apps/oeva/app/app/loading.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+import {Block, Preloader} from "konsta/react"
+import type {JSX} from "react";
+
+export default function Loading(): JSX.Element {
+    return <Block className="text-center">
+        <Preloader/>
+    </Block>
+}

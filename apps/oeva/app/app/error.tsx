1633f403c10d58467e2ad591aa15120a8e349b50
robertkleinschuster <git@robs.social>
2023-12-13 18:59:21 +0100

    error page improvements

--- apps/oeva/app/app/error.tsx
+++ apps/oeva/app/app/error.tsx
@@ -1,11 +1,24 @@
 "use client"
 
-import { Block, Button } from "konsta/react"
-import { useRouter } from "next/navigation"
+import {Card, Link} from "konsta/react"
+import {useRouter} from "next/navigation"
 import React from "react"
 
 export default function Error(): React.JSX.Element {
     const router = useRouter()
 
-    return <Block>Es ist ein Fehler aufgetreten.<Button clear onClick={() => { router.back() }}>Zurück</Button></Block>
+    return <Card
+        footer={<div className="flex gap-4">
+            <Link onClick={() => {
+                router.back()
+            }}>Zurück</Link>
+            <Link onClick={() => {
+                router.refresh()
+            }}>Neu Laden</Link>
+        </div>}
+        header="Ups..."
+        outline
+    >
+        Es ist ein Fehler aufgetreten.
+    </Card>
 }

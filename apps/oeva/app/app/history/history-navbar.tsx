1d5d182fefe959d62308edbc484ce2d0d941402b
robertkleinschuster <git@robs.social>
2023-12-13 21:30:15 +0100

    bulk delete history items - toolbar

--- apps/oeva/app/app/history/history-navbar.tsx
+++ apps/oeva/app/app/history/history-navbar.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import {Button, Icon, Navbar} from "konsta/react";
+import {Button, Icon, Navbar, Toolbar} from "konsta/react";
 import type {JSX} from "react";
 import {useState} from "react";
 import {Trash} from "framework7-icons/react"
@@ -15,22 +15,34 @@ export function HistoryNavbar(): JSX.Element {
     const [confirmDelete, setConfirmDelete] = useState(false)
     return <>
         <Navbar
-            right={edit ? <span className="flex gap-1">
-                {edit.length ? <Button onClick={() => {setConfirmDelete(true)}} className="bg-red-700 flex gap-1"><Icon ios={<Trash/>}/>Löschen</Button> : null}
-                <Button clear onClick={() => {dispatchEdit(false)}}>Abbrechen</Button>
-            </span> : <Button clear onClick={() => {dispatchEdit(true)}}>Bearbeiten</Button>}
+            right={edit ?
+                <Button clear onClick={() => {
+                    dispatchEdit(false)
+                }}>Abbrechen</Button>
+                : <Button clear onClick={() => {
+                    dispatchEdit(true)
+                }}>Bearbeiten</Button>}
             title="Verlauf"
         />
-        <ConfirmDialog onConfirm={() => {
-            if (edit) {
+        {edit ? <Toolbar className="bottom-0 fixed z-30">
+            <span className="flex gap-1">
+                <Button className="bg-red-700 text-red-500 flex gap-1" disabled={edit.length === 0} onClick={() => {
+                    setConfirmDelete(true)
+                }} tonal><Icon ios={<Trash/>}/>Löschen</Button>
+            </span>
+        </Toolbar> : null}
+        {edit ?
+            <ConfirmDialog onConfirm={() => {
                 edit.forEach(item => {
                     hideInRecents(item.id)
                 })
-            }
-            setConfirmDelete(false)
-            dispatchEdit(false)
-        }} onDismiss={() => {
-            setConfirmDelete(false)
-        }} opened={confirmDelete} title={edit?.length === 1 ? <>&bdquo;{edit[0]?.title}&ldquo; löschen?</> : <>{edit?.length} Einträge löschen?</>}/>
+                setConfirmDelete(false)
+                dispatchEdit(false)
+            }} onDismiss={() => {
+                setConfirmDelete(false)
+            }} opened={confirmDelete}
+                           title={edit.length === 1 ? <>&bdquo;{edit[0]?.title}&ldquo; löschen?</> :
+                               <>{edit.length} Einträge löschen?</>}/>
+            : null}
     </>
 }

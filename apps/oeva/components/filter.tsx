80a27576eb1f32de719f78bcf0c2b65dab95faf6
robertkleinschuster <git@robs.social>
2024-01-14 11:16:47 +0100

    add +/- 5 minutes to time filter

--- apps/oeva/components/filter.tsx
+++ apps/oeva/components/filter.tsx
@@ -1,11 +1,23 @@
 "use client"
 
-import type { ProductType } from "hafas-client";
-import { Button, Icon, List, ListButton, ListInput, ListItem, Popover, Toggle, Toolbar } from "konsta/react";
-import { Clock, Calendar, PlusCircle, MinusCircle } from "framework7-icons/react"
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import React, { useEffect, useState } from "react";
-import { addDays, addHours, subDays, subHours } from "date-fns";
+import type {ProductType} from "hafas-client";
+import {
+    Button,
+    Icon,
+    List,
+    ListButton,
+    ListInput,
+    ListItem,
+    Popover,
+    Segmented,
+    SegmentedButton,
+    Toggle,
+    Toolbar
+} from "konsta/react";
+import {Clock, Calendar, PlusCircle, MinusCircle} from "framework7-icons/react"
+import {usePathname, useRouter, useSearchParams} from "next/navigation";
+import React, {useEffect, useState} from "react";
+import {addDays, addHours, addMinutes, subDays, subHours, subMinutes} from "date-fns";
 import Product from "./product";
 import Time from "./time";
 
@@ -31,7 +43,10 @@ function toDateLocal(date: Date): string {
     return `${YYYY}-${MM}-${DD}`;
 }
 
-export default function Filter({ products, showTime = false }: { products: readonly ProductType[], showTime?: boolean }): React.JSX.Element {
+export default function Filter({products, showTime = false}: {
+    products: readonly ProductType[],
+    showTime?: boolean
+}): React.JSX.Element {
     const router = useRouter()
     const searchParams = useSearchParams()
     const pathname = usePathname()
@@ -41,7 +56,9 @@ export default function Filter({ products, showTime = false }: { products: reado
     useEffect(() => {
         const newSearchParams = new URLSearchParams(searchParams)
         newSearchParams.delete('products')
-        productsFilter.forEach(product => { newSearchParams.append('products', product) })
+        productsFilter.forEach(product => {
+            newSearchParams.append('products', product)
+        })
         newSearchParams.set('when', when.toISOString())
         router.replace(`${pathname}?${newSearchParams.toString()}`)
     }, [pathname, router, searchParams, when, productsFilter])
@@ -52,75 +69,101 @@ export default function Filter({ products, showTime = false }: { products: reado
     return <>
         <Toolbar innerClassName="gap-2 !justify-start" top>
             {showTime ?
-                <Button className="filter-when !w-auto" onClick={() => { setWhenOpen(true) }} rounded tonal>
-                    <Icon ios={<Clock />} />&nbsp;<Time time={when} />
+                <Button className="filter-when !w-auto" onClick={() => {
+                    setWhenOpen(true)
+                }} rounded tonal>
+                    <Icon ios={<Clock/>}/>&nbsp;<Time time={when}/>
                 </Button>
                 :
-                <Button className="filter-when !w-auto" onClick={() => { setWhenOpen(true) }} rounded tonal>
-                    <Icon ios={<Calendar />} />&nbsp;{when.toLocaleDateString(['de'], { dateStyle: 'short' })}
+                <Button className="filter-when !w-auto" onClick={() => {
+                    setWhenOpen(true)
+                }} rounded tonal>
+                    <Icon ios={<Calendar/>}/>&nbsp;{when.toLocaleDateString(['de'], {dateStyle: 'short'})}
                 </Button>
             }
 
-            <Button className="filter-products !w-auto gap-1" onClick={() => { setProductsOpen(true) }} rounded tonal>
+            <Button className="filter-products !w-auto gap-1" onClick={() => {
+                setProductsOpen(true)
+            }} rounded tonal>
                 <span>{productsFilter.size ? 'Verkehrsmittel:' : 'Verkehrsmittel wählen'}</span>
                 {products.filter(product => productsFilter.has(product.id)).map(product =>
-                    <Product key={product.id} product={product} />
+                    <Product key={product.id} product={product}/>
                 )}
             </Button>
         </Toolbar>
 
         <Popover
-            onBackdropClick={() => { setWhenOpen(false) }}
+            onBackdropClick={() => {
+                setWhenOpen(false)
+            }}
             opened={whenOpen}
             target=".filter-when"
         >
 
             {showTime ?
-                <List nested>
-                    <ListInput
-                        onChange={(e: Event) => { setWhen(new Date((e.target as HTMLInputElement).value)) }}
-                        outline
+                <div className="gap-1 flex flex-col m-4">
+                    <input
+                        className="border-none bg-transparent self-center"
+                        onChange={(e) => {
+                            setWhen(new Date((e.target as HTMLInputElement).value))
+                        }}
                         type="datetime-local"
                         value={toDatetimeLocal(when)}
                     />
-                    <ListButton onClick={() => {
+
+                    <Button onClick={() => {
                         setWhen(new Date())
-                    }} >Jetzt</ListButton>
-                    <ListButton onClick={() => {
-                        setWhen(subHours(when, 1))
-                    }} >
-                        <Icon
-                            ios={<MinusCircle />}
-                        />&nbsp;1 Stunde</ListButton>
-                    <ListButton onClick={() => {
-                        setWhen(addHours(when, 1))
-                    }} >
-                        <Icon
-                            ios={<PlusCircle />}
-                        />&nbsp;1 Stunde</ListButton>
-                </List>
+                    }}>Jetzt</Button>
+
+                    <Segmented>
+                        <SegmentedButton
+                            onClick={() => {
+                                setWhen(subHours(when, 1))
+                            }}
+                        >- 1 std</SegmentedButton>
+                        <SegmentedButton
+                            onClick={() => {
+                                setWhen(addHours(when, 1))
+                            }}
+                        >+ 1 std</SegmentedButton>
+                    </Segmented>
+                    <Segmented>
+                        <SegmentedButton
+                            onClick={() => {
+                                setWhen(subMinutes(when, 5))
+                            }}
+                        >- 5 min</SegmentedButton>
+                        <SegmentedButton
+                            onClick={() => {
+                                setWhen(addMinutes(when, 5))
+                            }}
+                        >+ 5 min</SegmentedButton>
+                    </Segmented>
+                </div>
                 :
                 <List nested>
                     <ListInput
-                        onChange={(e: Event) => { setWhen(new Date((e.target as HTMLInputElement).value)) }}
+                        onChange={(e: Event) => {
+                            setWhen(new Date((e.target as HTMLInputElement).value))
+                        }}
                         outline
                         type="date"
                         value={toDateLocal(when)}
                     />
                     <ListButton onClick={() => {
                         setWhen(new Date())
-                    }} >Heute</ListButton>
+                    }}>Jetzt</ListButton>
                     <ListButton onClick={() => {
                         setWhen(subDays(when, 1))
-                    }} >
+                    }}>
                         <Icon
-                            ios={<MinusCircle />}
+                            ios={<MinusCircle/>}
                         />&nbsp;1 Tag</ListButton>
                     <ListButton onClick={() => {
                         setWhen(addDays(when, 1))
-                    }} >
+                    }}>
                         <Icon
-                            ios={<PlusCircle />}
+                            ios={<PlusCircle/>}
                         />&nbsp;1 Tag</ListButton>
                 </List>
             }
@@ -128,7 +171,9 @@ export default function Filter({ products, showTime = false }: { products: reado
 
 
         <Popover
-            onBackdropClick={() => { setProductsOpen(false) }}
+            onBackdropClick={() => {
+                setProductsOpen(false)
+            }}
             opened={productsOpen}
             target=".filter-products"
         >
@@ -147,7 +192,7 @@ export default function Filter({ products, showTime = false }: { products: reado
                     }
                     key={product.id}
                     label
-                    media={<Product product={product} />}
+                    media={<Product product={product}/>}
                     title={product.name}
                 />)}
             </List>

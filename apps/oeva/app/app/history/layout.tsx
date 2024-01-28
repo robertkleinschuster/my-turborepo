cae884a1ad2c5243fe21834405f8811ae352a8b6
robertkleinschuster <git@robs.social>
2024-01-28 12:11:33 +0100

    loading for history and settings

--- apps/oeva/app/app/history/layout.tsx
+++ apps/oeva/app/app/history/layout.tsx
@@ -1,13 +1,16 @@
 "use client"
 
 import type {JSX, ReactNode} from "react";
+import { Suspense} from "react";
 import Scroll from "../../../components/scroll";
 import {HistoryNavbar} from "./history-navbar";
 import {HistorySelectionProvider} from "./history-selection-context";
 
 export default function Layout({children}: { children: ReactNode }): JSX.Element {
     return <HistorySelectionProvider>
-        <HistoryNavbar/>
+        <Suspense>
+            <HistoryNavbar/>
+        </Suspense>
         <Scroll>
             {children}
         </Scroll>
--- apps/oeva/app/app/settings/layout.tsx
+++ apps/oeva/app/app/settings/layout.tsx
@@ -1,12 +1,15 @@
 "use client"
 
 import type {JSX, ReactNode} from "react";
+import { Suspense} from "react";
 import Scroll from "../../../components/scroll";
 import {SettingsNavbar} from "./settings-navbar";
 
 export default function Layout({children}: { children: ReactNode }): JSX.Element {
     return <>
-        <SettingsNavbar/>
+        <Suspense>
+            <SettingsNavbar/>
+        </Suspense>
         <Scroll>
             {children}
         </Scroll>
